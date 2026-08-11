9978c58e3c5380d4fc569d89875454ec44f0c2f1
Genaro López Lap <genaro.lopezgr@uanl.edu>
2026-08-11 14:05:47 -0600

    Update

---
+++ Scripts/Primer_Script_proyecto.R
@@ -0,0 +1,15 @@
+# Genaro López Grimmaldo
+# 1280362
+# 11/08/2026
+
+
+# Ingesar Datos --------
+
+altura <- 12
+12 + altura
+altura * altura
+altura + dbh
+# Conjunto de datos
+
+dbh <- c(12, 10, 11.5, 13.2, 18.5)
+# Estimar medias -------
